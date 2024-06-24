def827c4099e1135ab1922cc5b2a153a179fb956
Alberto Perez <apo0106@gmail.com>
2024-06-24 13:02:43 +0200

    Responsive changes

--- footer.php
+++ footer.php
@@ -1,18 +1,19 @@
-    <!-- FOOTER -->
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 mt-auto border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-      <span class="text-muted">© 2024 Made by <a href="">Alberto Perez</a></span>
+<!-- FOOTER -->
+<footer class="d-flex flex-column flex-md-row flex-wrap justify-content-between justify-content-sm-center align-items-center py-3 my-4 mt-auto border-top">
+    <div class="col-md-4 d-flex align-items-center mb-3 mb-md-0">
+        <a href="/" class="me-2 text-muted text-decoration-none lh-1">
+            <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+        <span class="text-muted">© 2024 Made by <a href="">Alberto Perez</a></span>
     </div>
-    <span class="text-muted"> This  items & prices correspond with  World of Warcraft Cataclysm classic - Gehennas EU</span>  
+    <span class="text-muted text-center mb-3 mb-md-0"> This items & prices correspond with World of Warcraft Cataclysm classic - Gehennas EU</span>  
 
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex me-2">
+    <ul class="nav col-md-4 justify-content-center justify-content-md-center list-unstyled d-flex me-2">
         <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-linkedin" style="font-size: 24px;"></i></a></li>
         <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-github" style="font-size: 24px;"></i></a></li>
     </ul>
-  </footer>
+</footer>
+
  <!-- Cargar jQuery primero -->
  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Luego jQuery UI -->
--- index.php
+++ index.php
@@ -17,56 +17,56 @@
     <!-- MAIN -->
     <div class="container-fluid content">
         <div class="row">
-            <div class="col-md-2 col-sm-12 aside d-flex flex-column justify-content-flex-end" style="padding-top: 42px">
-            <table class="table caption-top items-table table-striped table-hover text-center mt-4 py-1" id="top10">
-            <caption>Top 10 glyphs</caption>
-                        <thead class="">
-                            <tr>
-                                <th scope="col" class="w-60">Item</th>
-                                <th scope="col" class="w-40">Ink</th>
-                            </tr>
-                        </thead>
+            <div class="col-md-3 col-sm-12 aside d-flex flex-column justify-content-flex-end" style="padding-top: 42px">
+                <table class="table caption-top items-table table-striped table-hover text-center mt-4 py-1" id="top10">
+                    <caption>Top 10 glyphs</caption>
+                    <thead>
+                        <tr>
+                            <th scope="col" class="w-60">Item</th>
+                            <th scope="col" class="w-40">Ink</th>
+                        </tr>
+                    </thead>
                     <tbody></tbody>
                 </table>
-                <table class="table  caption-top items-table table-striped table-hover text-center mt-auto" id="cheapestHerbs">
-                <caption>Cheapest herb for Inks</caption>
-                        <thead class="">
-                            <tr>
-                                <th scope="col" class="w-60">Ink</th>
-                                <th scope="col" class="w-40">Herb</th>
-                            </tr>
-                        </thead>
+                <table class="table caption-top items-table table-striped table-hover text-center mt-auto" id="cheapestHerbs">
+                    <caption>Cheapest herb for Inks</caption>
+                    <thead>
+                        <tr>
+                            <th scope="col" class="w-60">Ink</th>
+                            <th scope="col" class="w-40">Herb</th>
+                        </tr>
+                    </thead>
                     <tbody></tbody>
                 </table>
             </div>
               
-            <div class="col-md-10 col-sm-12">
+            <div class="col-md-9 col-sm-12">
                 <div class="row mt-2">
-                    <div class="col-md-2 col-sm-12">
+                    <div class="col-lg-2 col-md-4 col-sm-6 col-12">
                         <div class="d-flex align-items-center mb-2 mb-lg-0">
                             <label for="typeSelector" class="me-2">Type:</label>
                             <select id="typeSelector" class="form-select"></select>
                         </div>
                     </div>
-                    <div class="col-md-8 col-sm-12 d-flex justify-content-center">
+                    <div class="col-lg-8 col-md-8 col-sm-12 d-flex justify-content-center">
                         <div class="input-group date-picker">
-                            <button class=" index-btn btn btn-outline-secondary" type="button" id="prev-day">←</button>
+                            <button class="index-btn btn btn-outline-secondary" type="button" id="prev-day">←</button>
                             <input type="date" class="form-control text-center" id="date-picker">
-                            <button class=" index-btn btn btn-outline-secondary" type="button" id="next-day">→</button>
+                            <button class="index-btn btn btn-outline-secondary" type="button" id="next-day">→</button>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-12 table-container mb-4">
                         <table class="table items-table table-striped table-hover text-center mt-4" id="itemsTable">
-                            <thead class="">
+                            <thead>
                                 <tr>
                                     <th scope="col" class="w-5">#</th>
                                     <th scope="col" class="w-25">Item</th>
                                     <th scope="col" class="w-15">Average Price</th>
                                     <th scope="col" class="w-15">Price</th>
-                                    <th scope="col" class="w-15">Difference with Yesterday</th>
-                                    <th scope="col" class="w-15">Profit</th>
+                                    <th scope="col" class="w-20">Difference</th>
+                                    <th scope="col" class="w-10">Profit</th>
                                     <th scope="col" class="w-10">Sold</th>
                                 </tr>
                             </thead>
