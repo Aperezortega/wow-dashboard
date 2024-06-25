7f3e4652b318c67a763ffc50a54c0725f819e7a1
Alberto Perez <apo0106@gmail.com>
2024-06-25 10:58:17 +0200

    changed title and added hyperlink

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <!-- NAVBAR -->
 <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid justify-content-between">
-            <a class="navbar-brand" href="#">Inscription Dashboard</a>
+            <a class="navbar-brand fw-bolder" href="index.php">Profession Dashboard: Inscription</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
